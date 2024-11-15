acdacd1fa18461ffc0c548d4cb013548c3a50923
ValentinaC <v.cifuentes04@ufromail.cl>
2024-11-15 11:40:35 -0300

    se convierte el numero dado en una lista

--- Taller_4/src/main/java/com/example/taller_4/Taller4Application.java
+++ Taller_4/src/main/java/com/example/taller_4/Taller4Application.java
@@ -3,11 +3,26 @@ package com.example.taller_4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Taller4Application {
-
+	List <Integer> numerosOriginales;
 	public static void main(String[] args) {
 		SpringApplication.run(Taller4Application.class, args);
 	}
 
+	public int krapekarOp(int num) {
+		numerosOriginales = new ArrayList<>();
+		numerosOriginales.addAll(String.valueOf(Math.abs(num))
+				.chars()
+				.mapToObj(c -> c - '0')
+				.collect(Collectors.toList()));
+	return num
+	}
+
 }
+
+
