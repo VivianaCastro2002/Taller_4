e93c6be014f7eebfca8db91286823a32aa421ffe
vivivi <v.castro05@ufromail.cl>
2024-11-15 11:14:11 -0300

    se agregan las pruebas unitarias

---
+++ Taller_4/src/main/java/com/example/taller_4/Taller4Application.java
@@ -0,0 +1,13 @@
+package com.example.taller_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller4Application.class, args);
+	}
+
+}
